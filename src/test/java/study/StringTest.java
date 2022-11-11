0fd7d069a72861ab79432a30961ee8080c6194cd
leesh1993 <dltjdgnsok1@naver.com>
2022-11-11 10:23:16 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +12,33 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void test1() {
+        String data1 = "1,2";
+        String[] splitData1 = data1.split(",");
+        assertThat(splitData1).contains("1", "2");
+
+        String data2 = "1";
+        String[] splitData2 = data2.split(",");
+        assertThat(splitData2).containsExactly("1");
+    }
+
+    @Test
+    void test2() {
+        String data1 = "(1,2)";
+        String substring = data1.substring(1,4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 테스트")
+    void test3() {
+        String data1 = "abc";
+        int length = data1.length();
+        assertThatThrownBy(() -> data1.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: %d", length);
+    }
+
+
 }
